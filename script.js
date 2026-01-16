2cf47b82a555fee689974876202ef6757aa5755a
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-16 17:47:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// SingVerse Global JavaScript console.log('SingVerse JS Loaded');
-
-// Smooth scroll function function smoothScroll(target) { document.querySelector(target).scrollIntoView({ behavior: 'smooth' }); }
-
-// Example function for future use function showAlert(msg) { alert(msg); }
