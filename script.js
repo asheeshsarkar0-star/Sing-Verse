4e59d5c76fcb7d92cf49195d40947d9155e2c913
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-16 11:53:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// SingVerse Global JavaScript console.log('SingVerse JS Loaded');
+
+// Smooth scroll function function smoothScroll(target) { document.querySelector(target).scrollIntoView({ behavior: 'smooth' }); }
+
+// Example function for future use function showAlert(msg) { alert(msg); }
